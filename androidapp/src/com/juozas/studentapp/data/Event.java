2063eed44195e66d8b32c9ce9d40930d1c205fab
Juozas Kaziukenas <juozas@juokaz.com>
2010-12-18 16:22:58 +0200

    Added start and end ints

--- androidapp/src/com/juozas/studentapp/data/Event.java
+++ androidapp/src/com/juozas/studentapp/data/Event.java
@@ -13,18 +13,23 @@ public class Event {
 	private String Location;
 	private String Alts;
 	
+	private int StartInt;
+	private int EndInt;
+	
 	private static String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
 	private static String[] times = {"09.00", "10.00", "11.10", "12.10", "13.05", "14.00", "15.00", "16.10", "17.10"}; 
 	private static int[] minutes = {  540,     600,     670,     730,     780,     840,     900,     970,     1030};
 	private static String[] sites = {     "central", "external", "kb", "med+vet", "other"};
 	private static String[] sitesNames = {"CA",      "EX",       "KB", "CA",      "OT"};
 	
-	public Event(String day, String start, int duration, String location, String alts) {
+	public Event(String day, String start, int startint, int endint, int duration, String location, String alts) {
 		Day = day;
 		Start = start;
 		Duration = duration;
 		Location = location;
 		Alts = alts;
+		StartInt = startint;
+		EndInt = endint;
 	}
 
 	public static ArrayList<Event> factory(String start, String end, String sites, String alts) {
@@ -67,7 +72,7 @@ public class Event {
 					", start time: " + start_time + ", duration: " + Integer.toString(duration) + ", site: " + site +
 					", alts: " + alternative);
 			
-			events.add(new Event(day, start_time, duration, site, alternative));
+			events.add(new Event(day, start_time, start_, end_, duration, site, alternative));
 		}
 		
 		return events;
@@ -106,5 +111,21 @@ public class Event {
 
 	public void setDay(String day) {
 		Day = day;
+	}
+
+	public int getStartInt() {
+		return StartInt;
+	}
+
+	public void setStartInt(int startInt) {
+		StartInt = startInt;
+	}
+
+	public int getEndInt() {
+		return EndInt;
+	}
+
+	public void setEndInt(int endInt) {
+		EndInt = endInt;
 	}
 }
